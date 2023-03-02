5ac36a4e6e0dfab13fdaae64c3406bbd0e53f528
illiniCloudDev <107890959+illiniCloudDev@users.noreply.github.com>
2023-03-02 01:54:47 -0600

    added the views and controller folers. Created the index.ejs. Awaiting to transition the original html file to ejs. Added new lines of code for server.js. New packages added.

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 const express = require('express')
 const app = express()
+const connectDB = require('./config/database')
+const homeRoutes = require('./routes/home')
+// const todoRoutes = require('./routes/todos')
+
+require('dotenv').config({path: './config/.env'})
+
+connectDB()
