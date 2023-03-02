0f9d78edefa1d772db5b7c951ffae1dbfbe4fa4e
illiniCloudDev <107890959+illiniCloudDev@users.noreply.github.com>
2023-03-02 01:21:52 -0600

    created database to enter new string for DB STRING in env file.

--- server.js
+++ server.js
@@ -1,13 +1,2 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+const express = require('express')
+const app = express()
