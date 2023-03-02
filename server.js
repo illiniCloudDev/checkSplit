4c9357af7ad98bb80ba6f94f35d32c4d9fe246d1
illiniCloudDev <107890959+illiniCloudDev@users.noreply.github.com>
2023-03-02 00:54:06 -0600

    developed the config folder for our datatbase. Added 3 more packages to easily transition our middleware. Changed the server.js file to complu with future work.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.sendFile(__dirname + '/index.html');
 });
 
 const port = 3000;
