15dc9413637adc2e0d33c4b89cebfe27b5b0b2ac
illiniCloudDev <107890959+illiniCloudDev@users.noreply.github.com>
2023-03-03 11:11:12 -0600

    chaged some variables. Modified server.js to nmeet new paths. Index.ejs is in process of being built. Public folder will be next to help rendering index.ejs

--- server.js
+++ server.js
@@ -7,3 +7,15 @@ const homeRoutes = require('./routes/home')
 require('dotenv').config({path: './config/.env'})
 
 connectDB()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use('/', homeRoutes)
+// app.use('/todos', todoRoutes)
+ 
+app.listen(process.env.PORT, ()=>{
+    console.log('Server is running, you better catch it!')
+})    
